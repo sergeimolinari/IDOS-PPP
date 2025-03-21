a002dd57acbb1ea64f7d890827837a0b19d50a78
sergeimolinari <s73smoli@uni-bonn.de>
2025-03-21 14:34:28 +0100

    Version.

--- src/idos_ppp/_version.py
+++ src/idos_ppp/_version.py
@@ -5,10 +5,7 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple
-    from typing import Union
-
-    VERSION_TUPLE = Tuple[Union[int, str], ...]
+    VERSION_TUPLE = tuple[int | str, ...]
 else:
     VERSION_TUPLE = object
 
@@ -17,5 +14,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '3.1.dev1+g81d95ab.d20250306'
-__version_tuple__ = version_tuple = (3, 1, 'dev1', 'g81d95ab.d20250306')
+__version__ = version = "0.1.dev4+g1c19038"
+__version_tuple__ = version_tuple = (0, 1, "dev4", "g1c19038")
