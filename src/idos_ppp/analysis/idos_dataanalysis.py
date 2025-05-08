05989501ba75bc2758aef52161320a15cd359b15
sergeimolinari <s73smoli@uni-bonn.de>
2025-05-08 16:34:00 +0200

    Add correlation functions for prov part.

--- src/idos_ppp/analysis/idos_dataanalysis.py
+++ src/idos_ppp/analysis/idos_dataanalysis.py
@@ -49,6 +49,50 @@ def calculate_yearly_prot_prov_continent_correlations(data):
 
     return correlations_df
 
+# Correlation Analysis -> Function: Create a function to calculate and visualize the correlation between provision and participation.
+
+
+def calculate_yearly_prov_part_correlations(data):
+    """Calculate the yearly correlations between provision, described as the socio-economic measure, and (political) participation (3-1 is the Electoral Democracy Index and 3-2 is Voice and Accountability ).
+
+    Arguments: - data: pd.DataFrame containing the data.
+
+    Returns:
+    pd.DataFrame: A DataFrame with the yearly correlations.
+    """
+    _fail_if_not_dataframe(data)
+    _fail_if_empty_dataframe(data)
+
+    yearly_data = data.groupby("year")[["provision", "participation"]]
+
+    yearly_correlations = yearly_data.apply(lambda x: x.corr().iloc[0, 1])
+
+    yearly_correlations_df = pd.DataFrame(
+        {"year": yearly_correlations.index, "correlation": yearly_correlations.values}
+    )
+
+    return yearly_correlations_df
+
+
+def calculate_yearly_prov_part_continent_correlations(data):
+    """Calculate the yearly correlation between provision and participation for each continent.
+
+    Arguments: - data: pd.DataFrame containing the data.
+
+    Returns:
+    pd.DataFrame: A DataFrame with the yearly correlations for each continent.
+    """
+    _fail_if_not_dataframe(data)
+    _fail_if_empty_dataframe(data)
+
+    grouped_data = data.groupby(["year", "continent"])[["provision", "participation"]]
+
+    correlations = grouped_data.apply(lambda x: x.corr().iloc[0, 1])
+
+    correlations_df = correlations.reset_index()
+    correlations_df.columns = ["year", "continent", "correlation"]
+
+    return correlations_df
 
 # Error handling functions
 def _fail_if_not_dataframe(data):
