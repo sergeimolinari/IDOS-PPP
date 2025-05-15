88b76b15d36015b6069145aa9880d3ffe5840125
sergeimolinari <s73smoli@uni-bonn.de>
2025-05-15 16:48:54 +0200

    Adjust plotting.

--- src/idos_ppp/final/idos_plot.py
+++ src/idos_ppp/final/idos_plot.py
@@ -9,32 +9,6 @@ pd.options.plotting.backend = "plotly"
 from idos_ppp.parameters import three_p_indexes, years, country_lists
 
 
-'''
-def plot_mean_trends(data, output_dir):
-    """Plot the trend and stores the plots in png format of the mean values for the 3P indexes over the years.
-
-    Arguments:
-    - data: pd.DataFrame containing the mean statistics data.
-    - output_dir: Path, directory to save the output plots.
-    """
-    _fail_if_not_dataframe(data)
-    _fail_if_empty_dataframe(data)
-
-    for index in three_p_indexes:
-        plt.figure(figsize=(12, 6))
-
-        sns.lineplot(data=data, x="year", y=index, hue="continent", marker="o")
-
-        plt.xlabel("Year")
-        plt.ylabel("Mean Value")
-        plt.title(f"Trend of {index.capitalize()} Over Years")
-        plt.legend(title="Continent")
-
-        output_file = output_dir / f"{index}_mean_trend.png"
-        plt.savefig(output_file)
-        plt.close()
-'''
-
 def plot_boxplots(data, output_dir):
     """
     Plot boxplots for each of the 3P indexes over the years and save the plots in png format.
@@ -62,7 +36,6 @@ def plot_boxplots(data, output_dir):
         plt.savefig(output_png_file_path)
         plt.close()
 
-# PROBLEMMMMMMMMM
 
 # Error handling functions
 def _fail_if_not_dataframe(data):
