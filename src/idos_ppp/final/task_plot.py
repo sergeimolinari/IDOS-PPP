3558312351889936e9aedc6ce67707e7bf99c92b
sergeimolinari <s73smoli@uni-bonn.de>
2025-05-16 16:55:16 +0200

    Check problem correlation heatmaps.

--- src/idos_ppp/final/task_plot.py
+++ src/idos_ppp/final/task_plot.py
@@ -3,8 +3,8 @@
 import pandas as pd
 
 from idos_ppp.config import BLD
-from idos_ppp.final.idos_plot import plot_boxplots
-from idos_ppp.parameters import country_lists, three_p_indexes
+from idos_ppp.final.idos_plot import plot_boxplots, plot_correlation_heatmap
+from idos_ppp.parameters import country_lists, three_p_indexes, years
 
 inputs_plots = {
     list_name: BLD / "data" / "subsets" / f"{list_name}_data.pkl"
@@ -13,23 +13,53 @@ inputs_plots = {
 inputs_plots["merged_dataframe_countries"] = BLD / "data" / "merged_data.pkl"
 
 
+inputs_stat_plots = {
+    list_name: BLD / "analysis" / "statistical_analysis" / f"{list_name}_yearly_statistics.pkl"
+    for list_name in country_lists.keys()
+}
+inputs_stat_plots["merged_dataframe_yearly_statistics"] = BLD / "analysis" / "statistical_analysis" / "merged_dataframe_yearly_statistics.pkl"
+
+
 # Boxplots -> Function: Create a function to create a boxplot for each of the 3P indexes over the years.
 
 
-products_plots = []
+products_boxplots = []
 for list_name in inputs_plots:
     for index in three_p_indexes:
-        products_plots.append(
+        products_boxplots.append(
             BLD / "final" / "boxplots" / f"{list_name}" / f"{index}_boxplot.png",
         )
 
 
 def task_plot_boxplots(
-    stat_values=inputs_plots,
-    produces=products_plots,
+    input_values=inputs_plots,
+    produces=products_boxplots,
 ):
     """Task to plot boxplots for each of the 3P indexes over the years."""
-    for list_name, stat_data_path in stat_values.items():
-        stat_data = pd.read_pickle(stat_data_path)
+    for list_name, data_path in input_values.items():
+        data = pd.read_pickle(data_path)
         output_png_file_path = BLD / "final" / "boxplots" / f"{list_name}"
-        plot_boxplots(stat_data, output_png_file_path)
+        plot_boxplots(data, output_png_file_path)
+
+
+# Heatmaps -> Function: Create a function to create a heatmap for the correlation of the 3P indexes over the years.
+
+# PROBLEMMMMMMMM
+
+products_correlation_heatmap = []
+for list_name in inputs_stat_plots:
+    for year in years:
+        products_correlation_heatmap.append(
+            BLD / "final" / "correlation_heatmaps" / f"{list_name}" / f"{year}_correlation_heatmap.png",
+        )
+
+
+def task_plot_correlation_heatmap(
+    input_values=inputs_plots,
+    produces=products_correlation_heatmap,
+):
+    """Task to plot correlation heatmap for each dataset over the years."""
+    for list_name, data_path in input_values.items():
+        data = pd.read_pickle(data_path)
+        output_png_file_path = BLD / "final" / "correlation_heatmaps" / f"{list_name}"
+        plot_correlation_heatmap(data, output_png_file_path)
