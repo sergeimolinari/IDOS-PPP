d9b8cacac34bc3e971126731b9786efaa2257240
sergeimolinari <s73smoli@uni-bonn.de>
2025-05-20 17:32:50 +0200

    Add comparison between Yemen and Lebanon.

--- src/idos_ppp/final/task_plot.py
+++ src/idos_ppp/final/task_plot.py
@@ -1,10 +1,15 @@
-"""Task running the results formatting (plots of trend by 3p index)."""
+"""Tasks for plotting the data."""
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
 from idos_ppp.config import BLD
-from idos_ppp.final.idos_plot import plot_boxplots, plot_correlation, plot_comparative_bar_chart, plot_interactive_plots
+from idos_ppp.final.idos_plot import (
+    plot_boxplots,
+    plot_comparative_bar_chart,
+    plot_correlation,
+    plot_interactive_plots,
+)
 from idos_ppp.parameters import country_lists, three_p_indexes, years
 
 inputs_plots = {
@@ -15,22 +20,46 @@ inputs_plots["merged_dataframe_countries"] = BLD / "data" / "merged_data.pkl"
 
 
 inputs_prot_prov_correlations = {
-    list_name: BLD / "analysis" / "prot_prov_correlations" / f"{list_name}_yearly_prot_prov_correlations.pkl"
+    list_name: BLD
+    / "analysis"
+    / "prot_prov_correlations"
+    / f"{list_name}_yearly_prot_prov_correlations.pkl"
     for list_name in country_lists.keys()
 }
-inputs_prot_prov_correlations["merged_dataframe_countries"] = BLD / "analysis" / "prot_prov_correlations" / "merged_dataframe_yearly_prot_prov_correlations.pkl"
+inputs_prot_prov_correlations["merged_dataframe_countries"] = (
+    BLD
+    / "analysis"
+    / "prot_prov_correlations"
+    / "merged_dataframe_yearly_prot_prov_correlations.pkl"
+)
 
 inputs_prov_part_correlations = {
-    list_name: BLD / "analysis" / "prov_part_correlations" / f"{list_name}_yearly_prov_part_correlations.pkl"
+    list_name: BLD
+    / "analysis"
+    / "prov_part_correlations"
+    / f"{list_name}_yearly_prov_part_correlations.pkl"
     for list_name in country_lists.keys()
 }
-inputs_prov_part_correlations["merged_dataframe_countries"] = BLD / "analysis" / "prov_part_correlations" / "merged_dataframe_yearly_prov_part_correlations.pkl"
+inputs_prov_part_correlations["merged_dataframe_countries"] = (
+    BLD
+    / "analysis"
+    / "prov_part_correlations"
+    / "merged_dataframe_yearly_prov_part_correlations.pkl"
+)
 
 inputs_prot_part_correlations = {
-    list_name: BLD / "analysis" / "prot_part_correlations" / f"{list_name}_yearly_prot_part_correlations.pkl"
+    list_name: BLD
+    / "analysis"
+    / "prot_part_correlations"
+    / f"{list_name}_yearly_prot_part_correlations.pkl"
     for list_name in country_lists.keys()
 }
-inputs_prot_part_correlations["merged_dataframe_countries"] = BLD / "analysis" / "prot_part_correlations" / "merged_dataframe_yearly_prot_part_correlations.pkl"
+inputs_prot_part_correlations["merged_dataframe_countries"] = (
+    BLD
+    / "analysis"
+    / "prot_part_correlations"
+    / "merged_dataframe_yearly_prot_part_correlations.pkl"
+)
 
 
 # Boxplots -> Function: Create a function to create a boxplot for each of the 3P indexes over the years.
@@ -40,7 +69,11 @@ products_boxplots = []
 for list_name in inputs_plots:
     for index in three_p_indexes:
         products_boxplots.append(
-            BLD / "final" / "boxplots" / f"{list_name}" / f"{index.capitalize()}_boxplot.png",
+            BLD
+            / "final"
+            / "boxplots"
+            / f"{list_name}"
+            / f"{index.capitalize()}_boxplot.png",
         )
 
 
@@ -61,7 +94,11 @@ def task_plot_boxplots(
 products_prot_prov_correlations = []
 for list_name in inputs_prot_prov_correlations:
     products_prot_prov_correlations.append(
-        BLD / "final" / "prot_prov_correlations" / f"{list_name}" / f"{list_name}_correlation.png",
+        BLD
+        / "final"
+        / "prot_prov_correlations"
+        / f"{list_name}"
+        / f"{list_name}_correlation.png",
     )
 
 
@@ -79,7 +116,11 @@ def task_plot_prot_prov_correlations(
 products_prov_part_correlations = []
 for list_name in inputs_prov_part_correlations:
     products_prov_part_correlations.append(
-        BLD / "final" / "prov_part_correlations" / f"{list_name}" / f"{list_name}_correlation.png",
+        BLD
+        / "final"
+        / "prov_part_correlations"
+        / f"{list_name}"
+        / f"{list_name}_correlation.png",
     )
 
 
@@ -97,7 +138,11 @@ def task_plot_prov_part_correlations(
 products_prot_part_correlations = []
 for list_name in inputs_prot_part_correlations:
     products_prot_part_correlations.append(
-        BLD / "final" / "prot_part_correlations" / f"{list_name}" / f"{list_name}_correlation.png",
+        BLD
+        / "final"
+        / "prot_part_correlations"
+        / f"{list_name}"
+        / f"{list_name}_correlation.png",
     )
 
 
@@ -118,12 +163,19 @@ def task_plot_prot_part_correlations(
 products_comparative_bar_charts = []
 for year in years:
     products_comparative_bar_charts.append(
-        BLD / "final" / "lebanon_and_yemen" / "comparative_bar_charts" / f"bar_chart_{year}.png",
+        BLD
+        / "final"
+        / "lebanon_and_yemen"
+        / "comparative_bar_charts"
+        / f"bar_chart_{year}.png",
     )
 
 
 def task_plot_comparative_bar_charts(
-    input_data=BLD / "data" / "subsets" / "conflict_and_postconflict_countries_data.pkl",
+    input_data=BLD
+    / "data"
+    / "subsets"
+    / "conflict_and_postconflict_countries_data.pkl",
     produces=products_comparative_bar_charts,
 ):
     """Task to plot comparative bar charts for each dataset over the years."""
@@ -140,8 +192,15 @@ def task_plot_comparative_bar_charts(
 
 
 def task_plot_interactive_plots(
-    input_data=BLD / "data" / "subsets" / "conflict_and_postconflict_countries_data.pkl",
-    produces=BLD / "final" / "lebanon_and_yemen" / "interactive_plots" / "conflict_and_postconflict_countries_interactive_plot.html",
+    input_data=BLD
+    / "data"
+    / "subsets"
+    / "conflict_and_postconflict_countries_data.pkl",
+    produces=BLD
+    / "final"
+    / "lebanon_and_yemen"
+    / "interactive_plots"
+    / "conflict_and_postconflict_countries_interactive_plot.html",
 ):
     """Task to plot interactive plots for each dataset over the years."""
     data = pd.read_pickle(input_data)
