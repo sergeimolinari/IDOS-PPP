8cf9daa3fa1d491c7cd079e95b4714600ad5232d
sergeimolinari <s73smoli@uni-bonn.de>
2025-05-20 15:07:58 +0200

    Add tests for scatterplots.

--- tests/final/test_plot.py
+++ tests/final/test_plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from src.idos_ppp.final.idos_plot import plot_boxplots
+from src.idos_ppp.final.idos_plot import plot_boxplots, plot_correlation
 from src.idos_ppp.parameters import three_p_indexes
 
 
@@ -20,6 +20,15 @@ def sample_data():
 
 
 @pytest.fixture
+def sample_correlation_data():
+    data = {
+        "year": [2007, 2010, 2013, 2016, 2019],
+        "correlation": [0.1, 0.3, 0.5, 0.7, 0.9]
+    }
+    return pd.DataFrame(data)
+
+
+@pytest.fixture
 def output_dir(tmp_path):
     # temporary path
     return tmp_path
@@ -28,7 +37,7 @@ def output_dir(tmp_path):
 def test_boxplot(sample_data, output_dir):
     plot_boxplots(sample_data, output_dir)
     for index in three_p_indexes:
-        output_file = output_dir / f"{index}_boxplot.png"
+        output_file = output_dir / f"{index.capitalize()}_boxplot.png"
         assert output_file.exists()
 
 
@@ -50,9 +59,28 @@ def test_boxplot_missing_columns(sample_data, output_dir):
         plot_boxplots(data_missing_columns, output_dir)
 
 
-def test_boxplot_correct_plot_titles(sample_data, output_dir):
-    plot_boxplots(sample_data, output_dir)
-    for index in three_p_indexes:
-        output_file = output_dir / f"{index}_boxplot.png"
-        assert output_file.exists()
-        # Optionally, you can add more assertions to check the content of the plot titles
+def test_plot_correlation(sample_correlation_data, output_dir):
+    plot_correlation(sample_correlation_data, output_dir)
+    output_file = output_dir / f"{output_dir.name}_correlation.png"
+    assert output_file.exists()
+
+
+def test_plot_correlation_error_handling(output_dir):
+    empty_data = pd.DataFrame()
+    with pytest.raises(ValueError):
+        plot_correlation(empty_data, output_dir)
+
+
+def test_plot_correlation_missing_columns(sample_correlation_data, output_dir):
+    data_missing_columns = sample_correlation_data.drop(columns=["correlation"])
+    with pytest.raises(KeyError):
+        plot_correlation(data_missing_columns, output_dir)
+
+
+def test_plot_correlation_highlighting(sample_correlation_data, output_dir):
+    sample_correlation_data.loc[len(sample_correlation_data)] = [2022, 0.1]
+    sample_correlation_data.loc[len(sample_correlation_data)] = [2023, 0.9]
+
+    plot_correlation(sample_correlation_data, output_dir)
+    output_file = output_dir / f"{output_dir.name}_correlation.png"
+    assert output_file.exists()
